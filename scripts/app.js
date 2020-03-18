f48bb629c8bde5485c8eca2bdabfccd33b97b5b8
kadrianne <kristine.a.du@gmail.com>
2020-03-18 12:28:00 -0600

    Build webpage using API.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
 //your code here
+
+fetch('https://telephone-assessment.herokuapp.com/api/v1/contacts')
+    .then(response => response.json())
+    .then(parsedResponse => {
+        const characters = parsedResponse.data
+        showCharacters(characters)
+    })
+
+const charactersList = document.querySelector('#characters')
+
+function showCharacters(characters){
+    characters.map(character => {
+        createCharacterItem(character)
+    })
+}
+
+function createCharacterItem(character){
+    const characterItem = document.createElement('li')
+    characterItem.innerHTML = `
+        <img src='${character.imageURL}'>
+        <span>${character.name} - ${character.phone}</span>
+        <p>${character.message}</p>
+        <a href='/contact.html?character=${character.name}'>Leave ${character.name} a message.</a>`
+    charactersList.append(characterItem)
+}
